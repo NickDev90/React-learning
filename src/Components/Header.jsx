aafbe859e366a7d3defaec27c494c2df68f4aac7
NickDev90 <nickie05021990@gmail.com>
2020-10-18 20:17:15 +0200

    Uploading files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+	return <header className="header">
+        <img src="https://a.storyblok.com/f/47007/410x200/d1811e7e9b/joom-200x100.png" alt=""/>
+      </header>
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+	return <div className='content'>
+      <div> 
+        <img src="https://asiatec.sg/wp-content/uploads/2016/09/nature-1000x600.jpg" alt=""/>
+      </div>
+      <div>
+        ava + descr.
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          Post 1
+        </div>  
+        <div>
+          Post 2
+        </div>  
+        <div>
+          Post 3
+        </div>  
+      </div>
+    </div>
+
+}
+
+export default Profile;
