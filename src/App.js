3c748eb4b773cf12df8da5101bd045bc84100cb7
NickDev90 <nickie05021990@gmail.com>
2020-10-20 20:30:03 +0200

    new folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header.jsx';
-import Navbar from './Components/Navbar.jsx';
-import Profile from './Components/Profile.jsx';
+import Header from './Components/Header/Header.jsx';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import Profile from './Components/Profile/Profile.jsx';
 
 
 
