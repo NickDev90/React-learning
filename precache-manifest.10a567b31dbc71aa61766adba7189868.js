d06ae321b4b6e45c1d5b0a9cb72b9844cb218fff
NickDev90 <nickie05021990@gmail.com>
2021-07-11 18:22:22 +0200

    Updates

---
+++ precache-manifest.10a567b31dbc71aa61766adba7189868.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16886aa93f4245593f28b796e321cafc",
+    "url": "/React-learning/index.html"
+  },
+  {
+    "revision": "e0aa7ac49033af36dfcd",
+    "url": "/React-learning/static/css/3.ee6830e3.chunk.css"
+  },
+  {
+    "revision": "108bab001cbd9b7adcec",
+    "url": "/React-learning/static/css/4.8679924e.chunk.css"
+  },
+  {
+    "revision": "c908de85e400b27aec7c",
+    "url": "/React-learning/static/css/main.c4dd8ff7.chunk.css"
+  },
+  {
+    "revision": "43fc05f04e5497b14330",
+    "url": "/React-learning/static/js/2.a2290b53.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-learning/static/js/2.a2290b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0aa7ac49033af36dfcd",
+    "url": "/React-learning/static/js/3.eff37102.chunk.js"
+  },
+  {
+    "revision": "108bab001cbd9b7adcec",
+    "url": "/React-learning/static/js/4.70b5bcc9.chunk.js"
+  },
+  {
+    "revision": "14ecb9e367acdbca89a5",
+    "url": "/React-learning/static/js/5.ead32c70.chunk.js"
+  },
+  {
+    "revision": "c908de85e400b27aec7c",
+    "url": "/React-learning/static/js/main.81786f77.chunk.js"
+  },
+  {
+    "revision": "3703c43c32222dfb6f83",
+    "url": "/React-learning/static/js/runtime-main.2fea79cc.js"
+  },
+  {
+    "revision": "899048ab0cc455154006fdb9676964b3",
+    "url": "/React-learning/static/media/user-young-boy.899048ab.jpg"
+  }
+]);
